16ec0ce456a7dc88895c9a7ac023513115450f9b
james <james@jamesekstrom.com>
2021-02-09 22:48:43 -0800

    Run npm install.

--- src/com/prj/ReactjsBuild.groovy
+++ src/com/prj/ReactjsBuild.groovy
@@ -3,6 +3,7 @@ package com.prj
 def build(pipelineVars) {
     echo "Running ${pipelineVars.buildArgs}"
     dir("${pipelineVars.buildArgs.reactjs.name}") {
+        sh 'npm install'
         sh 'npm run-script build'
     }
 }
